0ad7ca2ab23702f4bdc183e66b7d34f1b777db21
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-02-10 13:09:58 +0600

    update_all_section_004

--- src/Pages/All/All.jsx
+++ src/Pages/All/All.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './All.css'
 import Allsign from './Allsection/Allsign/Allsign'
+import Alldigital from './Allsection/Alldigital/Alldigital'
 const All = () => {
   return (
    <section id='all'>
       <Allsign/>
       <div className='all_pages_wrapper'>
-        
+        <Alldigital/>
       </div>
    </section>
   )
--- src/Pages/All/Allsection/Alldigital/Alldigital.jsx
+++ src/Pages/All/Allsection/Alldigital/Alldigital.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import './Alldigital.css'
+import Nanohead from '../../../../Component/Nanohead/Nanohead'
+import { Link } from 'react-router-dom'
 
+let digitallink = [
+    {
+        id : 1,
+        name : "Bajar Music",
+        link : "bajarmusic"
+    },
+    {
+        id : 2,
+        name : "Kindle E-Readers & Books ",
+        link : "kindlereaders"
+    },
+    {
+        id :3 ,
+        name : "Bajar Appstore",
+        link : "bajarappstore"
+    },
+]
 const Alldigital = () => {
   return (
     <section id='alldigital'>
         <div className='alldigital_wrapper'>
             <div className='alldigital_wrapper_head_box'>
-                
+                <Nanohead text="Digital Content & Devices" style="alldigital_wrapper_head"/>
             </div>
+            <ul className='alldigital_wrapper_link_box'>
+                {
+                    digitallink.map((item , index)=>(
+                        <li className='alldigital_wrapper_alllink_item_box' key={index}>
+                        <Link className='alldigital_alllinks' to ={item.link}>{item.name}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     </section>
   )
